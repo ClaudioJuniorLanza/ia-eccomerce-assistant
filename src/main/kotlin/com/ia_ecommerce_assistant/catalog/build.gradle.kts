43bf4347939fd2fcae3f36c3a0f7368c91abb38e
Claudio <clanca@uolinc.com>
2025-07-15 21:21:31 -0300

    Adiciona Java 21 e libs iniciais do quarkus

--- src/main/kotlin/com/ia_ecommerce_assistant/catalog/build.gradle.kts
+++ src/main/kotlin/com/ia_ecommerce_assistant/catalog/build.gradle.kts
@@ -3,6 +3,15 @@ plugins {
     id("io.quarkus") version "3.24.0"
 }
 
+kotlin {
+    jvmToolchain(21)
+    compilerOptions {
+        progressiveMode = true
+        freeCompilerArgs = listOf("-Xjsr305=strict", "-Xjdk-release=21")
+        allWarningsAsErrors = true
+    }
+}
+
 group = "com.ia_ecommerce_assistant.catalog"
 version = "1.0.0-SNAPSHOT"
 
@@ -14,16 +23,23 @@ repositories {
 dependencies {
     implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:3.24.0"))
     implementation(kotlin("stdlib-jdk8"))
-    implementation("io.quarkus:quarkus-resteasy-reactive-jackson")
+    implementation("io.quarkus:quarkus-rest-kotlin-serialization")
+    implementation("io.quarkus:quarkus-kotlin")
     implementation("io.quarkus:quarkus-arc")
     testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.rest-assured:rest-assured")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+tasks.withType<JavaCompile>().configureEach {
+    sourceCompatibility = "21"
+    targetCompatibility = "21"
 }
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
 
 quarkus {
     // Adicione configurações específicas do Quarkus aqui, se necessário
